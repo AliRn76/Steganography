de3c50050adce69e6078b50674e95a341e595540
Ali <40032682+AliRn76@users.noreply.github.com>
2020-04-25 01:07:40 +0430

    decode.c Fixed with malloc

--- decode.c
+++ decode.c
@@ -10,7 +10,7 @@ int main( int argc, char *argv[] ) {
 	
 	if( argc == 2 ) {		
 		FILE *fptr;
-		unsigned char data[MAX_FILE_SIZE];
+		unsigned char* data = (char*)malloc(MAX_FILE_SIZE);
 		
 		fptr = fopen(argv[1], "rb");
 		unsigned char original_hex[] = "\x35\x51\x9D\x36\xE0\x80\x3F\x0A\xF4\x29\x6A\x8E\x69\xE8\xCA\xF3\x49\xBD\x76\x81\x81\x50\x18\xB0\x06\x2A\x7D\x87\x34\x87\x8A\xEC\x5A\x18\x32\x12\x9E\xB4\xCC\x0C\x75\xA7\xB9\x23"; 
@@ -21,9 +21,10 @@ int main( int argc, char *argv[] ) {
 		}
 		
 		fclose(fptr);
-
-		for(int i=880 ; i<924 ; i++)
-			key[i-880] = (data[i] ^ (original_hex[i-880]));
+		
+		int z;
+		for(z=880 ; z<924 ; z++)
+			key[z-880] = (data[z] ^ (original_hex[z-880]));
 			
 		
 	}
@@ -36,9 +37,10 @@ int main( int argc, char *argv[] ) {
 	
 	
 	printf("Key: ");
-
-	for(int i=0 ; i<44 ; i++)
-		printf("%c", key[i]);
+	
+	int j;
+	for(j=0 ; j<44 ; j++)
+		printf("%c", key[j]);
 	printf("\n");
 	
 	return 0;
