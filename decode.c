e1e1a5de2e7ea07d4c42bee8a6bc07158956d455
alirn76 <alirn76@yahoo.com>
2020-04-24 22:56:40 +0430

    fix some thing

--- decode.c
+++ decode.c
@@ -4,63 +4,27 @@
 
 
 
-void swap(int *xp, int *yp)
-{
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
-
-
-int togglebit(int n) 
-{ 
-    if (n == 0) 
-        return 1; 
-  
-    int i = n; 
-    n |= n >> 1; 
-    n |= n >> 2; 
-    n |= n >> 4; 
-    n |= n >> 8; 
-    n |= n >> 16; 
-  
-    return i ^ n; 
-} 
-
-int XNOR(int num1, int num2)
-{
-    if (num1 < num2)
-        swap(&num1, &num2);
-    num1 = togglebit(num1);
-
-    return num1 ^ num2;
-}
-
 int main( int argc, char *argv[] ) {
 
-	char key[1024];
+	char key[64];
 	
-	if( argc == 2 ) {
-		//printf("The arguments supplied are %s\n", argv[1]);
-		
+	if( argc == 2 ) {		
 		FILE *fptr;
 		unsigned char data[MAX_FILE_SIZE];
 		
 		fptr = fopen(argv[1], "rb");
 		unsigned char orginal_hex[] = "\x35\x51\x9D\x36\xE0\x80\x3F\x0A\xF4\x29\x6A\x8E\x69\xE8\xCA\xF3\x49\xBD\x76\x81\x81\x50\x18\xB0\x06\x2A\x7D\x87\x34\x87\x8A\xEC\x5A\x18\x32\x12\x9E\xB4\xCC\x0C\x75\xA7\xB9\x23"; 
+		
 		int i = 0;
 		while (!feof(fptr)) {
 			data[i++] = fgetc(fptr);
 		}
 		
 		fclose(fptr);
-		for(int i=880 ; i<924 ; i++){
-			//printf("DATA: %X ", data[i]);
-			//printf("Orginal_HEX: %X ", orginal_hex[i-880]);
-			//key[i-880] = XNOR((data[i]), orginal_hex[i-880]);
+
+		for(int i=880 ; i<924 ; i++)
 			key[i-880] = (data[i] ^ (orginal_hex[i-880]));
-			//printf("  KEY: %c\n", key[i-880]);
-		}
+			
 		
 	}
 	else if( argc > 2 ) {
@@ -72,6 +36,7 @@ int main( int argc, char *argv[] ) {
 	
 	
 	printf("Key: ");
+
 	for(int i=0 ; i<44 ; i++)
 		printf("%c", key[i]);
 	printf("\n");
--- encode.c
+++ encode.c
@@ -5,35 +5,20 @@
 
 int main( int argc, char *argv[] ) {
 
-	int z = 0;
-	int y = 0;
 	unsigned char argv_int[44];
 	char final_name[100] = "secured_";
-
-	//printf("sizeof(a) / sizeof(a[0]): %li\n", (sizeof(argv[2]) / sizeof(argv[2][0])));
-	//for(int i=0 ; i<44 ; i++){
-	//	argv_int[i] = argv[2][i];
-       	//	printf("%c", argv_int[i]);	
-	//}
-	while(argv[2][y]){
-		argv_int[y] = argv[2][y++];
-	}
 	
-	printf("\n");
-	
-	//for (int i=0 ; i<44 ; i++){
-		//printf("%i: %c    ",i, argv_int[i]);
-	//}
-	//printf("\n");
-	//while(argv[2][z])
-		//printf("argv[2][%i] = %c\n", z, argv[2][z++]);
-
 	if( argc == 3 ) {
-		//printf("The arguments supplied are %s and %s\n", argv[1], argv[2]);
-		
 		FILE *fptr;
 		char data[MAX_FILE_SIZE];
 		
+		int y = 0;
+		while(argv[2][y]){
+			if(y == 44)
+				break;
+			argv_int[y] = argv[2][y++];
+		}
+	
 		fptr = fopen(argv[1], "rb");
 		
 		int i = 0;
@@ -46,27 +31,27 @@ int main( int argc, char *argv[] ) {
 		fptr = fopen(strcat(final_name, argv[1]), "wb");
 		
 		for(int j = 0;j < i-1;j++){
-			if(j>=880 && j<924){
-				//if(argv_int[j-880]){
-					fputc((data[j] ^ argv_int[j-880]), fptr);
-					//printf("%c ^ %c = %c \n", data[j], argv_int[j-880], (data[j] ^ argv_int[j-880]));
-				//}
-				//fputc(data[j], fptr);
-				//printf("%c ", argv_int[j-880]);
-			}else
+			if(j>=880 && j<924)
+				fputc((data[j] ^ argv_int[j-880]), fptr);
+			else
 				fputc((data[j]),fptr);
 		}
+
 		fclose(fptr);
+
 		printf("Key Secured Successfully\n\n");	
-		printf("Your Key/Message:  %s\n", argv[2]);
-		printf("New Image Name:    %s\n", final_name);	
+		printf("Your Key/Message: ");
+		for(int i=0 ; i<44 ; i++)
+			printf("%c", argv_int[i]);
+		printf("\nNew Image Name:   %s\n", final_name);	
 	}
-	else if( argc > 3 ) {
+
+	else if( argc > 3 )
 		printf("Too many arguments supplied.\n");
-	}
-	else {
+
+	else
 		printf("Two argument expected.\n");
-	}
+
 	
 	return 0;
 }
